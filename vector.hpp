b87f71e954b91c39051d5611872e85070d40049b
WildBoarGonnaGo <60565993+WildBoarGonnaGo@users.noreply.github.com>
2021-05-28 17:03:00 +0300

    Add files via upload

--- vector.hpp
+++ vector.hpp
@@ -1,6 +1,7 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 # include <memory>
+# include <stdexcept>
 # ifdef __amd64__
 	typedef unsigned long	size_type;
 # else
@@ -85,11 +86,13 @@ namespace ft
 			size_type	i = 0;
 			
 			tmp = last - first;
-			if (_size < tmp)
+			if (_size < tmp && tmp < _capacity)
 			{
 				for (int i = _size - 1; i >= 0 ; --i)
+				{
 					alloc.destroy(_vector + i);
-					alloc.destroy(_vector + i);
+					alloc.constructor(_vector + i, *(first + i));
+				}
 				_size = last - first;
 			}
 			if (_capacity < tmp)
@@ -160,12 +163,142 @@ namespace ft
 					_alloc.destroy(_vector + idx);
 				_alloc.deallocate(_vector, _capacity);
 				_capacity = n;
+				_vector = allocTmp;
 			}
 		}
 		
 		size_type						capacity() const { return (_capacity); }
+
 		bool							empty() const { return (_size == 0); }
+
+		void							reserve(size_type n) {
+			if (n > _capacity)
+			{
+				T*	allocTmp = _alloc.allocate(n);
+				for (size_t i = 0; i < _size; i++)
+				{
+					_alloc.contructor(allocTmp + i, *(_vector + i)); 
+					_alloc.destroy(allocTmp + i);
+				}
+				_alloc.deallocate(_vector, _capacity);
+				_capacity = n;
+				_vector = allocTmp;
+			}
+		}
+
+		vector							&operator[](size_type n) {
+			return (*(_vector + n));
+		}
+
+		vector const					&operator[](size_type n) const {
+			return (*(_vector + n));
+		}
+
+		vector							&at(size_type n) {
+			if (n >= _size)
+				throw std::out_of_range();
+			return (_vector[n]);
+		}
+
+		vector const					&at(size_type n) const {
+			if (n >= _size)
+				throw std::out_of_range();
+			return (_vector[n]);
+		}
 		
+		vector							&front() {
+			return (_vector[0]);
+		}
+
+		vector const					&front(size_type n) const {
+			return (_vector[0]);
+		}
+
+		vector							&back() {
+			return (_vector[_size - 1]);
+		}
+
+		vector const					&back(size_type n) const {
+			return (_vector[_size - 1]);
+		}
+
+		void							assign(const_iterator first, const_iterator last) {
+			size_type	tempSize;
+			T*			allocTemp;
+
+			tempSize = (static_cast<int>(last - first) < 0) ?
+				first - last : last - first;
+			if (tempSize <= _capacity) {
+				for (size_t i = 0; i < tempSize; i++)
+				{
+					_alloc.destroy(_vector + i);
+					_alloc.contructor(_vector + i, *(first++))
+				}
+				if (tempSize > _size)
+					_size = tempSize;
+			} else {
+				allocTemp = _alloc.allocate(tempSize);
+				for (size_t i = 0; i < _size; i++)
+					_alloc.destroy(_vector + i);
+				_alloc.deallocate(_vector, _capacity);
+				for (size_t i = 0; i < tempSize; i++)
+					_alloc.contructor(_vector + i, *(first++));
+				_vector = allocTemp;
+			}
+		}
+
+		void							assign(size_type n, const T &val) {
+			T	*allocTemp;
+
+			if (n < _capacity) {
+				for (size_t i = 0; i < _size; i++)
+					_alloc.destroy(_vector + i);
+				for (size_t i = 0; i < n; i++)	
+					_alloc.constructor(_vector + i, val);
+				if (_size < n)
+					_size = n;
+			}
+			else {
+				allocTemp = _alloc.allocate(tempSize);
+				for (size_t i = 0; i < _size; i++)
+					_alloc.destroy(_vector + i);
+				_alloc.deallocate(_vector, _capacity);
+				for (size_t i = 0; i < tempSize; i++)
+					_alloc.contructor(_vector + i, val)
+				_vector = allocTemp;
+			}
+		}
+
+		void							push_back(const T &val) {
+			T*			allocTemp;
+
+			if (_size < _capacity)
+				_alloc.contructor(_vector + _size++, val);
+			else {
+				_capacity = 2 * _size;
+				allocTemp = _alloc.allocate(_capacity);
+				for (size_t i = 0; i < _size; i++) {
+					_alloc.contructor(allocTemp + i, _vector[i])
+					_alloc.destroy(_vector + i);
+				}
+				_alloc.deallocate(_vector, _capacity);
+				_alloc.contructor(allocTemp + _size++, val);
+				_vector = allocTemp;
+			}
+		}
+
+		void							pop_back() {
+			if (_size > 0) {
+				alloc.destroy(_vector + _size - 1);
+				--_size;
+			}
+				 
+		}	
+
+		iterator						insert(iterator position, const T &val) {
+			if 
+		}
+
 		~vector( ) {
 			for (int i = _size - 1; i >= 0 ; --i) {
 				_alloc.destroy(_vector + i);
